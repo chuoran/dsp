6e2ed8d73ba62dca75efdcad4f2fd6193d762468
Chuoran🦁 <Chuoran@Mao.local>
2017-09-07 12:11:49 -0700

    added three .py files created by three editors

---
+++ editors/egg.py
@@ -0,0 +1,4 @@
+print("scrambled eggs are nice but takes time to cook")
+
+print("I really need to familarize the mac keyboard")
+
---
+++ editors/vinegar.py
@@ -0,0 +1,9 @@
+import math
+
+a = int(input())
+arr = [int(n) for n in input().split()]
+
+print(a)
+print(arr)
+
+
